df5d2c9b09f3ef504314765ed0a162a023a9bc57
Roner1-bit <58177688+Roner1-bit@users.noreply.github.com>
2023-10-27 11:52:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from fastapi import FastAPI
-
-import logging
-import os
-import time
-
+from fastapi import FastAPI, Request, Response
+import json
 import torch
-from huggingface_hub import login
-from transformers import LlamaTokenizer, LlamaForCausalLM
-from llama_recipes.inference.model_utils import load_peft_model
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
@@ -15,11 +9,9 @@ from transformers import (
     HfArgumentParser,
     TrainingArguments,
     pipeline,
+    logging,
 )
 from peft import LoraConfig, PeftModel
-from trl import SFTTrainer
-
-torch.set_float32_matmul_precision("high")
 
 from api import (
     ProcessRequest,
@@ -28,33 +20,42 @@ from api import (
     TokenizeResponse,
     Token,
 )
-
-os.environ["HUGGINGFACE_TOKEN"] = "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"
-os.environ["HUGGINGFACE_REPO"] = "Roner1/LLaMA-Colosal-NeuroIPS"
-
-app = FastAPI()
-
-logger = logging.getLogger(__name__)
-# Configure the logging module
-logging.basicConfig(level=logging.INFO)
-
-login(token=os.environ["HUGGINGFACE_TOKEN"])
-
-# LoRA attention dimension
-lora_r = 64
-lora_alpha = 16
-lora_dropout = 0.1
-#4 bits quantization
-use_4bit = True
-bnb_4bit_compute_dtype = "float16"
-bnb_4bit_quant_type = "nf4"
-use_nested_quant = False
-
+import time
 
 model_name = "hpcai-tech/Colossal-LLaMA-2-7b-base"
 
-compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+################################################################################
+# QLoRA parameters
+################################################################################
 
+# LoRA attention dimension
+lora_r = 64
+
+# Alpha parameter for LoRA scaling
+lora_alpha = 16
+
+# Dropout probability for LoRA layers
+lora_dropout = 0.1
+
+################################################################################
+# bitsandbytes parameters
+################################################################################
+
+# Activate 4-bit precision base model loading
+use_4bit = True
+
+# Compute dtype for 4-bit base models
+bnb_4bit_compute_dtype = "float16"
+
+# Quantization type (fp4 or nf4)
+bnb_4bit_quant_type = "nf4"
+
+# Activate nested quantization for 4-bit base models (double quantization)
+use_nested_quant = False
+
+packing = False
+device_map = {"": 0}
+compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
 bnb_config = BitsAndBytesConfig(
     load_in_4bit=use_4bit,
     bnb_4bit_quant_type=bnb_4bit_quant_type,
@@ -69,94 +70,42 @@ if compute_dtype == torch.float16 and use_4bit:
         print("Your GPU supports bfloat16: accelerate training with bf16=True")
         print("=" * 80)
 
-
-
+# Load base model
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     quantization_config=bnb_config,
-    device_map="cuda"
+    device_map=device_map
 )
 model.config.use_cache = False
 model.config.pretraining_tp = 1
 
-
-model = load_peft_model(model, os.environ["HUGGINGFACE_REPO"])
-
-model.eval()
-
+# Load LLaMA tokenizer
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 tokenizer.pad_token = tokenizer.eos_token
-tokenizer.padding_side = "right" 
+tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
 
-LLAMA2_CONTEXT_LENGTH = 4096
+# Load LoRA configuration
+peft_config = LoraConfig(
+    lora_alpha=lora_alpha,
+    lora_dropout=lora_dropout,
+    r=lora_r,
+    bias="none",
+    task_type="CAUSAL_LM",
+)
 
+app = FastAPI()
 
 @app.post("/process")
-async def process_request(input_data: ProcessRequest) -> ProcessResponse:
-    if input_data.seed is not None:
-        torch.manual_seed(input_data.seed)
-    
-    encoded = tokenizer(input_data.prompt, return_tensors="pt")
-    
-    prompt_length = encoded["input_ids"][0].size(0)
-    max_returned_tokens = prompt_length + input_data.max_new_tokens
-    assert max_returned_tokens <= LLAMA2_CONTEXT_LENGTH, (
-        max_returned_tokens,
-        LLAMA2_CONTEXT_LENGTH,
-    )
-    print('moving to cuda')
-    model.cuda()
-    t0 = time.perf_counter()
-    encoded = {k: v.cuda() for k, v in encoded.items()}
-    with torch.no_grad():
-        outputs = model.generate(
-            **encoded,
-            max_new_tokens=input_data.max_new_tokens,
-            do_sample=True,
-            temperature=input_data.temperature,
-            top_k=input_data.top_k,
-            return_dict_in_generate=True,
-            output_scores=True,
-        )
-    
-    t = time.perf_counter() - t0
-    if not input_data.echo_prompt:
-        output = tokenizer.decode(outputs.sequences[0][prompt_length:], skip_special_tokens=True)
-    else:
-        output = tokenizer.decode(outputs.sequences[0], skip_special_tokens=True)
-        
-    tokens_generated = outputs.sequences[0].size(0) - prompt_length
-    logger.info(
-        f"Time for inference: {t:.02f} sec total, {tokens_generated / t:.02f} tokens/sec"
-    )
+async def answer_question(input_data: ProcessRequest) -> ProcessResponse:
 
-    logger.info(f"Memory used: {torch.cuda.max_memory_reserved() / 1e9:.02f} GB")
-    generated_tokens = []
-    
-    log_probs = torch.log(torch.stack(outputs.scores, dim=1).softmax(-1))
-
-    gen_sequences = outputs.sequences[:, encoded["input_ids"].shape[-1]:]
-    gen_logprobs = torch.gather(log_probs, 2, gen_sequences[:, :, None]).squeeze(-1)
-    
-
-    top_indices = torch.argmax(log_probs, dim=-1)
-    top_logprobs = torch.gather(log_probs, 2, top_indices[:,:,None]).squeeze(-1)
-    top_indices = top_indices.tolist()[0]
-    top_logprobs = top_logprobs.tolist()[0]
-
-    for t, lp, tlp in zip(gen_sequences.tolist()[0], gen_logprobs.tolist()[0], zip(top_indices, top_logprobs)):
-        idx, val = tlp
-        tok_str = tokenizer.decode(idx)
-        token_tlp = {tok_str: val}
-        generated_tokens.append(
-            Token(text=tokenizer.decode(t), logprob=lp, top_logprob=token_tlp)
-        )
-    logprob_sum = gen_logprobs.sum().item()
-    
-    return ProcessResponse(
-        text=output, tokens=generated_tokens, logprob=logprob_sum, request_time=t
-    )
-
+  prompt = input_data.prompt
+  print(prompt)
+  pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=200)
+  result = pipe(f"<s>[INST] {prompt} [/INST]")
+  print(result[0]['generated_text'])
+  answer = result[0]['generated_text']
+  response = json.dumps({"answer": answer})
+  return Response(content=response, media_type="application/json")
 
 @app.post("/tokenize")
 async def tokenize(input_data: TokenizeRequest) -> TokenizeResponse:
@@ -167,4 +116,5 @@ async def tokenize(input_data: TokenizeRequest) -> TokenizeResponse:
     t = time.perf_counter() - t0
     tokens = encoded["input_ids"]
     return TokenizeResponse(tokens=tokens, request_time=t)
-print("bye")
+
+print('bye')
